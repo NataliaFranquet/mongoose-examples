9968e2c7d020cc8097f57d35279e204c73ad9791
Dani Vicario <danigv.email@gmail.com>
2019-04-11 15:24:11 +0200

    updated callback hell

---
+++ callbackhell.js
@@ -0,0 +1,11 @@
+f1(function () {
+    f2(function () {
+        f3(function () {
+            f4(function () {
+                f5(function () {
+                    console.log("triangular code sucks")
+                })
+            })
+        })
+    })
+})
